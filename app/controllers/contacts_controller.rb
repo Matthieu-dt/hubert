968ab6858fce5070c3df8e29f8f74fd7ac24fc70
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2021-01-01 18:13:12 +0100

    contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,15 +5,21 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new(contact_params)
     authorize @contact
     @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
+      redirect_to root_path, notice: 'Message bien reçu je reviens vers vous dès que possible !'
     else
       flash.now[:error] = 'Cannot send message'
       render :new
     end
   end
 
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message, :nickname)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # get 'contact/new'
-    devise_for :users
+  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :categories do

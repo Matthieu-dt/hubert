4c424954fffda6f489ba1ae866380bdecd9b5348
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2021-01-01 18:25:27 +0100

    contact

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,14 +110,15 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'https://hubert-de-tarle.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  host = 'https://hubert-de-tarle.herokuapp.com' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
 
   ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
+  :user_name => ENV['GMAIL_USERNAME'],
+  :password => ENV['GMAIL_PASSWORD'],
+  :domain => 'gmail.com',
+  :address => 'smtp.gmail.com',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true

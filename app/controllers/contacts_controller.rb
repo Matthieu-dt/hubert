0f2f1f1488072e96ad96be640c31952bdfc0c1eb
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2021-01-01 17:12:15 +0100

    contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,12 +5,13 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(contact_params)
+    @contact = Contact.new(params[:contact])
+
     authorize @contact
     @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
-  redirect_to root_path, notice: 'Message sent successfully'
+      redirect_to root_path, notice: 'Message sent successfully'
     else
       flash.now[:error] = 'Cannot send message'
       render :new

3d609f703fe849c1cffc2b0466f182310ce3fc6b
Matthieu de Tarle <matthieu.de.tarle@gmail.com>
2021-01-01 17:46:47 +0100

    contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,7 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(contact_params)
+    @contact = Contact.new(params[:contact])
     authorize @contact
     @contact.request = request
     if @contact.deliver
@@ -17,9 +17,4 @@ class ContactsController < ApplicationController
     end
   end
 
-  private
-
-  def contact_params
-    params.require(:contact).permit(:name, :email, :message)
-  end
 end
